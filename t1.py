6b6b3a8ea7619073dd29a8168ef15f2474738e8f
KENW2019 <dapei.wong@gmail.com>
2019-03-06 15:25:18 +0800

    test write operation 2

--- t1.py
+++ t1.py
@@ -1,6 +1,14 @@
-with open('/home/k/share/Python/CANApp/test.c', 'w') as f:
+with open('/home/k/share/Python/test.c', 'w') as f:
 	#print(f.read())
 	#for line in f.readlines():
 		#print(line.strip())
 	f.write('static const TsCANApp_h_TxCanSigIf CtCANApp_h_TxCanSigIf[2u] =')
+
+
+with open('/home/k/share/Python/test.c', 'w') as f:
+	#print(f.read())
+	#for line in f.readlines():
+		#print(line.strip())
+	#f.write('static const TsCANApp_h_TxCanSigIf CtCANApp_h_TxCanSigIf[2u] =')
+	f.write('********************')
 	
