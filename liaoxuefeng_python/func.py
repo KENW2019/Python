9e26ff1c555844384b6de060f8600af508737abf
KENW2019 <dapei.wong@gmail.com>
2019-03-07 18:13:40 +0800

    test move

--- liaoxuefeng_python/func.py
+++ liaoxuefeng_python/func.py
@@ -1,16 +1,23 @@
 
 from func_myabs import my_abs
 from func_nop import nop
+from func_move import move
 
 '''print(my_abs(-1,2))
 print(nop())'''
 
 
-
+''' test func abs'''
+'''
 #my_abs('A')
 my_abs(-9)
 
 #abs('A')
+'''
+
+''' test func move'''
+r = move(1,2,3,)
+print(r)
 
 
 
