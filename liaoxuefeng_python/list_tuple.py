90f0783d8c0e59a9beb45a25d182fba5aba39a3a
KENW2019 <dapei.wong@gmail.com>
2019-03-07 14:00:34 +0800

    save temp

--- liaoxuefeng_python/list_tuple.py
+++ liaoxuefeng_python/list_tuple.py
@@ -4,6 +4,40 @@ classmates[0]
 
 classmates.append('adam')
 
-classmates.insert(1,'jack')
+classmates.insert(1, 'jack')
 
 L = ['Apple', 123, True]
+
+classmates = ('Michael', 'Bob', 'Tracy')
+
+L = [
+    ['Apple', 'Google', 'Microsoft'],
+    ['Java', 'Python', 'Ruby', 'PHP'],
+    ['Adam', 'Bart', 'Lisa']
+]
+
+print(L[0][0])
+
+age = 17
+if age >= 18:
+    print('your age is',age)
+else:
+    print('teenager')
+    
+
+age = 3
+if age >= 18:
+    print('adult')
+elif age >= 6:
+    print('teenager')
+else:
+    print('kid')
+
+
+age = 20
+if age >= 6:
+    print('teenager')
+elif age >= 18:
+    print(age)
+else:
+    print('kid')
