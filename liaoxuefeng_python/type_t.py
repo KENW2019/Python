5ed6d6f7bd645fe37288f51150dd710d996f1764
KENW2019 <dapei.wong@gmail.com>
2019-03-07 11:03:48 +0800

    save temp

--- liaoxuefeng_python/type_t.py
+++ liaoxuefeng_python/type_t.py
@@ -29,3 +29,18 @@ len('中文')
 b'\xe4\xb8\xad\xff'.decode('utf-8', errors='ignore')
 
 len('中文'.encode('utf-8'))
+
+
+'Hello, %s' % 'world'
+
+'hi, %s, you have $%d.' % ('michael',1000)
+
+print('%010d-%02d' % (3, 1))
+print('%.2f' % 3.1415926)
+
+'Age: %s. Gender: %s' % (25,True)
+
+s1 = 72
+s2 = 85
+r = ((85-72)/72)*100
+print('%.1f%%' % r)
