04b55e6213baa65ee2daaff3c51229d60311be20
KENW2019 <dapei.wong@gmail.com>
2019-03-07 19:18:31 +0800

    test func quadratic

--- liaoxuefeng_python/func.py
+++ liaoxuefeng_python/func.py
@@ -1,7 +1,7 @@
-
 from func_myabs import my_abs
 from func_nop import nop
 from func_move import move
+from func_quadratic import quadratic
 
 '''print(my_abs(-1,2))
 print(nop())'''
@@ -16,8 +16,18 @@ my_abs(-9)
 '''
 
 ''' test func move'''
+'''
 r = move(1,2,3,)
 print(r)
+
+x,y = move(1,2,3,)
+print(x,y)
+'''
+
+''' test func quadratic'''
+print('quadratic(2, 3, 1) =', quadratic(2, 3, 1))
+print('quadratic(1, 3, -4) =', quadratic(1, 3, -4))
+
 
 
 
