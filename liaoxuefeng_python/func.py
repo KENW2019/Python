11635b1455bdbf9fe57778ac66b632ff949b353f
KENW2019 <dapei.wong@gmail.com>
2019-03-07 17:39:06 +0800

    test

--- liaoxuefeng_python/func.py
+++ liaoxuefeng_python/func.py
@@ -2,8 +2,18 @@
 from func_myabs import my_abs
 from func_nop import nop
 
-print(my_abs(-1,2))
-print(nop())
+'''print(my_abs(-1,2))
+print(nop())'''
+
+
+
+#my_abs('A')
+my_abs(-9)
+
+#abs('A')
+
+
+
 
 
 
--- liaoxuefeng_python/func_myabs.py
+++ liaoxuefeng_python/func_myabs.py
@@ -1,8 +1,24 @@
 
-
+''' my_abs v1'''
+'''
 def my_abs(x):
     if x>=0:
         return x
     else:
         return -x
-        
+'''
+
+
+''' my_abs v1'''
+''' check input para'''
+def my_abs(x):
+    if not isinstance(x,(int,float)):
+        raise TypeError('bad operand type')
+    else:
+        if x >= 0:
+            y = x
+        else:
+            y = -x
+    print(y)
+    return y
+
