18506363ee6d0bdbda19227038e3dd0a94958449
KENW2019 <dapei.wong@gmail.com>
2019-03-10 11:04:49 +0800

    save current  student.py

--- liaoxuefeng_python/classTest/classTest.py
+++ liaoxuefeng_python/classTest/classTest.py
@@ -22,13 +22,31 @@ bart.print_score()
 print(bart.get_grade())
 '''
 
+'''
 lisa = student('wang dapei', 99)
 bart = student('yazhu', 59)
-print(lisa.name, lisa.get_grade())
-print(bart.name, bart.get_grade())
+print(lisa.get_grade())
+print(bart.get_grade())
 lisa.age = 18
 print(lisa.age)
-print(bart.age)
+#print(bart.age)
+'''
+'''
+kenw = student('wang dapei',99)
+print(kenw.get_name)
+print(kenw.get_score)
+print(kenw.get_name())
+print(kenw.get_score())
+'''
+
+kenw = student('wang dapei',99)
+print(kenw.get_score())
+kenw.set_score(43)
+print(kenw.get_score())
+
+
+
+
 
 
 
--- liaoxuefeng_python/classTest/student.py
+++ liaoxuefeng_python/classTest/student.py
@@ -1,4 +1,4 @@
-
+'''
 class student(object):
     def __init__(self,x,y):
         self.name = x
@@ -14,6 +14,34 @@ class student(object):
             return 'B'
         else:
             return 'C'
+'''
+
+class student(object):
+    def __init__(self,x,y):
+        self.__name = x
+        self.__score = y
+
+    def print_score(self):
+        print('%s: %s' % (self.__name, self.__score))
+
+    def get_grade(self):
+        if self.__score >= 90:
+            return 'A'
+        elif self.__score >= 60:
+            return 'B'
+        else:
+            return 'C'
+    
+    def get_name(self):
+        return self.__name
+    
+    def get_score(self):
+        return self.__score
+
+    def set_score(self, int_latestValue):
+        self.__score = int_latestValue
+
+
 
 
 
