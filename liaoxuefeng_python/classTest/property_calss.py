a9f6bcf1d238d9f7c61a28300d7b3f354899e38e
KENW2019 <dapei.wong@gmail.com>
2019-03-11 16:41:11 +0800

    save again for property

--- liaoxuefeng_python/classTest/property_calss.py
+++ liaoxuefeng_python/classTest/property_calss.py
@@ -12,4 +12,19 @@ class Student(object):
             raise ValueError('score must between 0 ~ 100')
         self._score = value
 
+    @property
+    def birth(self):
+        return self._birth
+    
+    @birth.setter
+    def birth(self,value):
+        self._birth = value
+    
+    @property
+    def age(self):
+        return 2015 - self._birth
+
+
+
+
 
