6487db5b355a92e212986436f55ff8e39aad784e
KENW2019 <dapei.wong@gmail.com>
2019-03-15 08:05:44 +0800

    save io test

--- liaoxuefeng_python/io_test/io_test.py
+++ liaoxuefeng_python/io_test/io_test.py
@@ -1,14 +1,16 @@
 
-file_handler = open('/home/k/LinuxShare/Python/liaoxuefeng_python/io_test/test.txt', 'r')
+file_handler = open('/home/k/LinuxShare/Python/liaoxuefeng_python/io_test/GB2312.txt', 'r', encoding = "gbk")
 
-'''
+
 str2 = file_handler.read()
 
 print(str2)
-'''
 
+
+'''
 for line in file_handler.readlines():
     print(line.strip())
+'''
 
 
 file_handler.close()
